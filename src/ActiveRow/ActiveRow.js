0ef04c5cbc5a76b0e06bd451351ed2a0eadfba3e
Alexey Volkov <al3xv0lk@yandex.ru>
2021-03-04 18:36:29 +0300

    first commit without style

---
+++ src/ActiveRow/ActiveRow.js
@@ -0,0 +1,26 @@
+
+import React from 'react';
+
+export default
+class ActiveRow extends React.Component{
+    
+
+    render(){
+        if(this.props.activeRow === null){
+            return <p>выбери ряд для вывода полной информации</p>
+        } else {
+            return (
+                <>  
+                Выбран пользователь <b>{this.props.activeRow.firstName+" "+this.props.activeRow.lastName}</b><br></br>
+                Описание:<br></br>
+                <textarea value={this.props.activeRow.description} readOnly/><br></br>
+                Адрес проживания: <b>{this.props.activeRow.address.streetAddress}</b><br></br>
+                Город: <b>{this.props.activeRow.address.city}</b><br></br>
+                Провинция/штат: <b>{this.props.activeRow.address.state}</b><br></br>
+                Индекс: <b>{this.props.activeRow.address.zip}</b>
+                    
+                   
+                </>
+        )}
+    }
+}
---
+++ src/AddRow/AddRow.js
@@ -0,0 +1,72 @@
+
+import React from 'react';
+
+export default
+class AddRow extends React.Component{
+    constructor(props) {
+        super(props);
+        this.state = {
+                    id: '',
+                    firstName: '',
+                    lastName: '',
+                    email: '',
+                    phone: '',
+                    address:{streetAddress:'нет информации'},
+                    description: 'нет информации'
+            
+        }   
+    }
+
+    handleChange = (event) => { 
+        let name = event.target.name; 
+            this.setState(() => {
+                return {
+                            [name]: event.target.value
+                        
+                }
+            });  
+        if(!document.forms.form.checkValidity()){
+            document.querySelector('.input-submit').setAttribute('disabled', true);
+        } else {
+            document.querySelector('.input-submit').removeAttribute('disabled');
+        }
+    }
+    
+    handleSubmit = (event) => {
+        event.preventDefault();
+        this.props.addRow(this.state);
+        document.forms.form.reset();
+        this.props.sortItem(null);
+
+      }
+
+    
+    render(){
+        return(
+            <form name='form' className="form" onSubmit={this.handleSubmit}>
+                <span> только цифры</span>
+                <label >id
+                    <input name="id" type="number" pattern="\d+"  required onChange={this.handleChange}/>
+                    
+                </label>
+                <span> только латиница</span>
+                <label >firstName
+                    <input name="firstName" type="text" pattern="[A-Za-z]+" required onChange={this.handleChange}/>
+                </label>
+                <span> только латиница</span>
+                <label>lastName
+                    <input name="lastName" type="text" pattern="[A-Za-z]+" required onChange={this.handleChange}/>
+                </label>
+                <span> формат: xxx@xxx.xx</span>
+                <label>email
+                    <input name="email" type="email" pattern="\w+@\w+\.[a-z]+" required onChange={this.handleChange}/>
+                </label>  
+                <span> формат: (xxx)xxx-xxxx</span>  
+                <label>phone
+                    <input name="phone" type="tel" pattern="\(\d{3}\)\d{3}-\d{4}" required onChange={this.handleChange}/>
+                </label>
+                <input  className="input-submit" type="submit" disabled={true} value="Добавить в таблицу"/>
+            </form>
+        
+        )}
+}
